7db1185afc4ddb6f430270d146f4dc4aa182de00
Karin Norrlander <karin@barprego.se>
2020-05-17 16:21:25 +0200

    further developmemt index + script

--- script.js
+++ script.js
@@ -12,23 +12,64 @@ fetch('https://localhost:5001/api/film')
     //sortera ut filmer som har mer än >= 1 i stock
 
 
-
+    //hämtar alla filmer och sätter igång en lista
     fetch('https://localhost:5001/api/film')
     .then(response => response.json())
-    .then(data => buildList(data));
+    .then(data => buildListOfMovies(data));
 
     
     //listar filmer på startsidan
-function buildList(Movies)
+function buildListOfMovies(Movies)
 {
+    //göra om för att återanvända listfunktionen
     document.getElementById("listOfMovies").innerHTML = "Följande filmer finns tillgängliga...";
     
     Movies.forEach(Movie => {
         var newDiv = document.createElement("div");
         newDiv.className = "addedDiv";
-        newDiv.textContent = "Titel: " + Movie.name + ", Id: " + Movie.id;
+        newDiv.textContent = "FilmId: " + Movie.id + " Titel: " + Movie.name + " Mer info här!"; 
 
         var containerDiv = document.getElementById("listOfMovies");
         containerDiv.appendChild(newDiv);
     });
+}
+
+function buildListOfTriva(id)
+{
+    //lägga in inpufält att välja vilken filmId man ska
+    //visa upp 
+    //se mer av 
+    //fetch trivia med specifikt id
+    //visar i diven 
+    //visar också bild i divven 
+}
+
+function welcomeStudio()
+{
+    //Hälsar studion välkommen
+    //låna filmer
+}
+
+//funktion för att ta bort film
+function deleteMovie(id)
+{
+    console.log("Raderar film " + id);
+
+    fetch('https://localhost:5001/api/film/' + id, {
+        method: 'DELETE', 
+    })
+    .then(response => response.json()) 
+}
+
+function addNewMovie(name, stock)
+{
+    var data = {name: name, stock: stock};
+    fetch('https://localhost:5001/api/film'), {
+        headers: { 'Content-Type': 'application/json',
+        method: 'POST',
+        body: JSON.stringify({
+          data
+        })
+        .then(response => response.json())}
+    }
 }
